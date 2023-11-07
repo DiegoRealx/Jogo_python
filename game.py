d5e6019406925a7981717779a09d3f45e2ad5dca
Diego Real <realdiego697@gmail.com>
2023-11-07 10:20:43 -0300

    atualização final

--- game.py
+++ game.py
@@ -1,65 +1,88 @@
+import json
 import random
 
+def ler_json(nome_arquivo):
+    try:
+        with open(nome_arquivo, 'r') as arquivo:
+            dados = json.load(arquivo)
+            return dados
+    except:
+        return { "jogadores": [], "pontuacao_media": 0 }
+
+def armazenar_ranking_json(ranking):
+    with open(nome_arquivo, "w") as arquivo:
+        jsonStr = json.dumps(ranking, indent = 4)
+        arquivo.write(jsonStr)
+        
+def inserir_ranking_jogador(rank_jogador, ranking):
+    tentativas_jogador = rank_jogador['tentativas']
+    possui_ranking = False
+    
+    if 'jogadores' in ranking == False: ranking['jogadores'] = []
+    
+    for rank in ranking['jogadores']:
+        if rank['nome'] == rank_jogador['nome']:
+            possui_ranking = True
+            if rank['tentativas'] > tentativas_jogador:
+                    rank['tentativas'] = tentativas_jogador
+    
+    if possui_ranking == False:
+        ranking['jogadores'].append(rank_jogador)
+              
+def calcular_media_tentativas(ranking):
+    total_tentativas = 0
+    
+    for rank in ranking['jogadores']: 
+        total_tentativas += rank['tentativas']
+    
+    total_jogadores = len(ranking['jogadores'])
+    media = total_tentativas / total_jogadores
+    return round(media, 2) 
+    
+nome_arquivo = 'ranking.json'
+
 numero_secreto = random.randint(1, 100)
-tentativas = 0
-ranking = []
+
+ranking = ler_json(nome_arquivo)
 
 nome = input('Olá! Qual é o seu nome? ')
 print(f'Olá {nome}! Bem-vindo ao jogo de adivinhação!')
 print('Tente adivinhar o número secreto entre 1 e 100!')
+
+tentativas = 0
 
 while True:
     try:
         tentativa = int(input('Digite um numero: '))
     except ValueError:
         print('Digite um numero valido')
-            
+
     tentativas += 1
 
-    if tentativa < numero_secreto:
-        print('O número é maior do que o que você tentou.')
-    elif tentativa > numero_secreto:
-        print('O número é menor do que o que você tentou.')
-    else:
-        print(f'Parabéns, {nome}! Você adivinhou o número corretamente: {numero_secreto} em {tentativas} tentativas.')
-
-        ranking.append((nome, tentativas))
-        with open('ranking.txt', 'a') as file:
-            file.write(f'{nome}: {tentativas} tentativas\n')
-
-        print('== RANKING ==')
-        ranking.sort(key=lambda x: x[1])
-        for i, (jogador, tentativas) in enumerate(ranking, start=1):
-            print(f'{i}. {jogador}: {tentativas} tentativas')
-        break
-
-try:
-    with open('ranking.txt', 'r') as file:
-        ranking = file.readlines()
+    if tentativa == numero_secreto:
+        print(f'Parabéns! Você acertou o número {numero_secreto} em {tentativas} tentativa(s).')
         
-        if len(ranking) == 0:
-            print('O ranking está vazio.')
-        else:
-            total_tentativas = 0
-            total_jogadores = 0
+        inserir_ranking_jogador({"nome": nome, "tentativas": tentativas}, ranking)
+        
+        jogadores_ordenados = sorted(ranking['jogadores'], key=lambda x: x['tentativas'])
+        ranking['jogadores'] = jogadores_ordenados
+        
+        media_pontuacao = calcular_media_tentativas(ranking)
+        ranking['pontuacao_media'] = media_pontuacao
+        
+        armazenar_ranking_json(ranking)
+        
+        print('=== RANKING ===')
+        for i, jogador in enumerate(ranking['jogadores']): 
+            print(f'{i + 1}. {jogador["nome"]}: {jogador["tentativas"]} tentativa(s)')
 
-            for linha in ranking:
-                jogador, tentativas_str = linha.split(":")
-                tentativas = int(tentativas_str.split()[0])
-                total_tentativas += tentativas
-                total_jogadores += 1
-                print(linha.strip())
-
-            media = total_tentativas / total_jogadores
-            print(f'A média de tentativas foi: {media:.2f}')
-            ranking.sort(key=lambda x: int(x.split(":")[1].split()[0]))
-
-            print('== RANKING FINAL ==')
-            for i, linha in enumerate(ranking, start=1):
-                print(f'{i}. {linha.strip()}')
-
-except FileNotFoundError:
-    print('Arquivo não encontrado.')
+        print(f'A pontuação média das tentativas dos jogadores é de: {media_pontuacao:}')
+                
+        break
+    elif tentativa < numero_secreto:
+        print(f'O número é maior do que {tentativa}.')
+    else:
+        print(f'O número é menor do que {tentativa}.')
 
 
 
