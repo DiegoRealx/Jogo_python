fca31eb9b00e0db0a254181dc7c49e16d69981be
Diego Real <realdiego697@gmail.com>
2023-11-10 19:16:16 -0300

    Atualização json

--- game.py
+++ game.py
@@ -18,7 +18,7 @@ def inserir_ranking_jogador(rank_jogador, ranking):
     tentativas_jogador = rank_jogador['tentativas']
     possui_ranking = False
     
-    if 'jogadores' in ranking == False: ranking['jogadores'] = []
+    if not 'jogadores' in ranking: ranking['jogadores'] = []
     
     for rank in ranking['jogadores']:
         if rank['nome'] == rank_jogador['nome']:
@@ -26,7 +26,7 @@ def inserir_ranking_jogador(rank_jogador, ranking):
             if rank['tentativas'] > tentativas_jogador:
                     rank['tentativas'] = tentativas_jogador
     
-    if possui_ranking == False:
+    if not possui_ranking:
         ranking['jogadores'].append(rank_jogador)
               
 def calcular_media_tentativas(ranking):
@@ -38,16 +38,23 @@ def calcular_media_tentativas(ranking):
     total_jogadores = len(ranking['jogadores'])
     media = total_tentativas / total_jogadores
     return round(media, 2) 
+
+def obter_nome_valido():
+    while True:
+        nome = input('Bem-vindo! Digite seu nome com 3 letras: ')
+        if len(nome) == 3 and nome.isalpha():
+            return nome.upper()
+        print('Por favor, digite apenas 3 letras.')
     
 nome_arquivo = 'ranking.json'
 
-numero_secreto = random.randint(1, 100)
+numero_secreto = random.randint(1, 50)
 
 ranking = ler_json(nome_arquivo)
 
-nome = input('Olá! Qual é o seu nome? ')
-print(f'Olá {nome}! Bem-vindo ao jogo de adivinhação!')
-print('Tente adivinhar o número secreto entre 1 e 100!')
+nome = obter_nome_valido()
+print(f'Olá, {nome}! Este é um jogo de adivinhação.')
+print('Tente adivinhar o número secreto entre 1 e 50!')
 
 tentativas = 0
 
@@ -56,11 +63,12 @@ while True:
         tentativa = int(input('Digite um numero: '))
     except ValueError:
         print('Digite um numero valido')
+        continue
 
     tentativas += 1
 
     if tentativa == numero_secreto:
-        print(f'Parabéns! Você acertou o número {numero_secreto} em {tentativas} tentativa(s).')
+        print(f'\nParabéns! Você acertou o número {numero_secreto} em {tentativas} tentativa(s).')
         
         inserir_ranking_jogador({"nome": nome, "tentativas": tentativas}, ranking)
         
@@ -76,20 +84,10 @@ while True:
         for i, jogador in enumerate(ranking['jogadores']): 
             print(f'{i + 1}. {jogador["nome"]}: {jogador["tentativas"]} tentativa(s)')
 
-        print(f'A pontuação média das tentativas dos jogadores é de: {media_pontuacao:}')
+        print(f'\nA pontuação média das tentativas dos jogadores é de: {media_pontuacao:}')
                 
         break
     elif tentativa < numero_secreto:
         print(f'O número é maior do que {tentativa}.')
     else:
         print(f'O número é menor do que {tentativa}.')
-
-
-
-
-
-
-#Jogo de adivinhação em que o jogador tenta adivinhar um número secreto escolhido aleatoriamente pelo computador usando a biblioteca random entre 1 e 100. O jogo começa solicitando ao jogador que insira seu nome e, em seguida, exibe uma mensagem de boas vidas personalizada.
-#Dentro de um loop principal, o jogo continua até que o jogador adivinhe o número correto. O jogador é solicitado a fazer palpite, e o número de tentativas é contado e selecionado conforme o jogador for errando. O jogador recebe dicas para adivinhar se o número secreto é maior ou menor que o palpite atual.
-#O jogo também mantém um ranking de jogadores, armazenando seus nomes e o número mínimo de tentativas possíveis para adivinhar o número. O ranking é atualizado após cada jogo.
-#No final do jogo mostra o ranking junto com a media de cada jogador
